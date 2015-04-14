dec2f3bc78cf12dd0067cc48c0c910b748f7c888
Forkk <forkk@forkk.net>
2015-04-13 20:03:47 -0500

    Switch from Xmobar to Taffybar

--- common/xmonad.nix
+++ common/xmonad.nix
@@ -9,19 +9,30 @@
     xorg.xmessage
     compton
     slock
+
+    haskellPackages.ghc
+    haskellPackages.xmonad
+    haskellPackages.taffybar
+    haskellPackages.xmobar # TODO: Switch to taffybar
   ];
 
   security.setuidPrograms = [ "slock" ];
 
   services.xserver = {
-    windowManager.default = "xmonad";
-    windowManager.xmonad = {
-      enable = true;
-      enableContribAndExtras = true;
+    windowManager = {
+      default = "xmonad";
+      xmonad = {
+        enable = true;
+        enableContribAndExtras = true;
+        extraPackages = haskellPackages: [
+          haskellPackages.taffybar
+        ];
+      };
     };
 
     displayManager = {
       sessionCommands = ''
+        eval `${pkgs.dbus_daemon}/bin/dbus-launch --auto-syntax`
         ${pkgs.xlibs.xset}/bin/xsetroot -cursor_name left_ptr
         ${pkgs.xlibs.xset}/bin/xset -dpms
         ${pkgs.xlibs.xset}/bin/xset s off
